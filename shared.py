d2e065865ae2a79a71c746e57c0c3d6f25480087
Sander Wegter <pilot008@gmail.com>
2020-12-07 10:23:02 +0100

    Added shared functions

--- shared.py
+++ shared.py
@@ -1,2 +1,5 @@
 def read_from_file(filename):
   return list(map(lambda x: x.rstrip('\n'), open(filename, 'r').readlines()))
+
+def read_from_file_grouped(filename):
+  return open(filename, 'r').read().split('\n\n')
