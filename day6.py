ed5082e81610bed9dc4ad6b98cb02a60a7f610db
Sander Wegter <pilot008@gmail.com>
2020-12-07 10:25:03 +0100

    Day 6

--- day6.py
+++ day6.py
@@ -7,10 +7,7 @@ def num_yes(forms):
   return sum([len(set(form_group.replace('\n', ''))) for form_group in forms])
 
 def num_all_yes(forms):
-  sum_yes = 0
-  for form_group in forms:
-    sum_yes += len(set.intersection(*[set(l) for l in form_group.split("\n")]))
-  return sum_yes
+  return sum([len(set.intersection(*[set(l) for l in form_group.split("\n")])) for form_group in forms])
 
 print("Part 1:", num_yes(custom_forms))
 print("Part 2:", num_all_yes(custom_forms))
